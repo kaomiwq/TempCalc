b6f60343bffafb56c33f7f570088f2dd4124b46c
kkkkvas <imkvas5659@gmail.com>
2024-11-29 00:12:10 +0300

    Add files via upload

---
+++ Nail/StaffPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nail
+{
+    /// <summary>
+    /// Логика взаимодействия для StaffPage.xaml
+    /// </summary>
+    public partial class StaffPage : Page
+    {
+        public StaffPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClientsButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ClientsPage());
+        }
+
+        private void ServiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ServicePage());
+        }
+
+        private void OrdersButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new OrdersPage());
+        }
+    }
+}
